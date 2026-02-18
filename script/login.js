70dee10d1a5596d72ef836e81bb1605ef3e51bbe
Mahin Abdullah <mahin.abdullah22@gmail.com>
2026-02-17 20:46:32 -0600

    added index functionality

--- script/login.js
+++ script/login.js
@@ -1,1 +1,22 @@
 console.log("hmmmmmmmmm");
+
+document.getElementById("login-btn").addEventListener("click", function () {
+  //1. get the mobile number input
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+
+  //2. get the pin input
+  const pinInput = document.getElementById("input-pin");
+  const pin = pinInput.value;
+
+  //3. match pin & mobile number
+  if (contactNumber === "01234567890" && pin === "1234") {
+    //3.1 true --> alert > homepage
+    alert("Login Success!");
+    window.location.assign("/home.html");
+  } else {
+    //3.2 false --> alert > return
+    alert("Login Failed!");
+    return;
+  }
+});
